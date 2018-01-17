28ed2ca92105b44baf9b0da0e0f012c01cddf7c4
mrasu <m.rasu.hitsuji@gmail.com>
2018-01-17 16:20:42 +0900

    Add integration test (#25)
    ### Purpose/goal of PR (Issue #)
    Add integration test

---
+++ integration_test/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once('WOVN.php/src/wovn_interceptor.php');
+?>
+<html>
+<head>
+<link rel="alternate" hreflang="en" href="http://ja.AAAAA.com/" />
+</head>
+<body>
+test
+</body>
+</html>
