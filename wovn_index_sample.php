0ad636fda83f8c49aa9564c8bf7d64d1695c39c8
Antoine DAVID <antoine@wovn.io>
2018-01-17 14:24:52 +0900

    adds security logic, warning on htaccess

--- wovn_index_sample.php
+++ wovn_index_sample.php
@@ -1,8 +1,11 @@
 <?php
 # Enable WOVN.php library
 require_once("WOVN.php/src/wovn_interceptor.php");
+# Check that the file is secure and
 # Load the static file if it exists
-if (file_exists(dirname(__FILE__) . $_SERVER["REQUEST_URI"])) {
-  include(dirname(__FILE__) . $_SERVER["REQUEST_URI"]);
+$real_final_path = realpath(dirname(__FILE__) . $_SERVER["REQUEST_URI"]);
+if (strpos($real_final_path, dirname(__FILE__)) !== false &&
+    file_exists($real_final_path)) {
+  include($real_final_path);
 }
 ?>
