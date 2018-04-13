efd84fae052d773236bcc21dda0ed7c5f9b2028b
mrasu <m.rasu.hitsuji@gmail.com>
2018-04-13 19:48:03 +0900

    Add sample for path pattern

--- wovn_index_sample.php
+++ wovn_index_sample.php
@@ -3,7 +3,12 @@
 require_once("WOVN.php/src/wovn_interceptor.php");
 # Check that the file is secure and
 # Load the static file if it exists
-$real_final_path = realpath(dirname(__FILE__) . $_SERVER["REQUEST_URI"]);
+$path = $_SERVER["REQUEST_URI"];
+
+# Uncomment below when you set "path" as url_pattern_name
+# $path = preg_replace('/^\/(?:ar|bg|zh-CHS|zh-CHT|da|nl|en|fi|fr|de|el|he|id|it|ja|ko|ms|my|ne|no|pl|pt|ru|es|sv|th|hi|tr|uk|vi)($|\/|\?)/', '$1', $path);
+
+$real_final_path = realpath(dirname(__FILE__) . $path);
 if (strpos($real_final_path, dirname(__FILE__)) !== false &&
     file_exists($real_final_path)) {
   include($real_final_path);
