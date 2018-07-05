c649e8fdfa741930cffae62d50724797aef7ed71
Adrien Bougouin <adrien@wovn.io>
2018-07-05 14:54:27 +0900

    Fix wovn_index_sample.php to not include query parameters during filepath lookup (#40)
    * Fix wovn_index_sample.php to not include query parameters during filepath lookup

--- hack/run_integration_test.sh
+++ hack/run_integration_test.sh
@@ -25,3 +25,26 @@ diff /tmp/result.txt integration_test/index_expected.html
 
 curl "localhost/amp.php" -o /tmp/result.txt
 diff /tmp/result.txt integration_test/amp_expected.html
+
+# STATIC CONTENT INTERCEPTION
+
+mod_rewrite_activation="a2enmod rewrite"
+if [ "${docker_name}" == "php:5.3-apache" ]; then
+  mod_rewrite_activation="${mod_rewrite_activation}; apache2 -D FOREGROUND"
+else
+  mod_rewrite_activation="${mod_rewrite_activation}; apache2-foreground"
+fi
+
+docker ps | grep -v CONTAINER | cut -d " " -f 1 | xargs docker kill || true
+
+docker run -d -p 80:80 -v $(pwd):/var/www/html/WOVN.php \
+                       -v $(pwd)/htaccess_sample:/var/www/html/.htaccess \
+                       -v $(pwd)/wovn_index_sample.php:/var/www/html/wovn_index.php \
+                       -v $(pwd)/integration_test:/var/www/html \
+                       ${docker_name} \
+                       /bin/bash -c "${mod_rewrite_activation}"
+
+waitServerUp
+
+curl "localhost/static.html?a=b" -o /tmp/result.txt
+diff /tmp/result.txt integration_test/static_expected.html
