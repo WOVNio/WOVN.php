99830fa4a62bb66ceb17d30b73c31d9cbecab338
Tim Mahrt <tim@wovn.io>
2018-08-23 09:55:08 +0900

    Change from realpath to our own library for sanitizing input
    realpath sanitizes the input by resolving . and .. but
    it also returns false, with no path, when the path does
    not exist.  But in the case of the code, it was searching
    the root directory for an index file, if the root directory
    did not exist.  Now it only searches the url given and yields
    a 404 error on error.

--- src/wovn_helper.php
+++ src/wovn_helper.php
@@ -3,11 +3,29 @@ require_once(__DIR__ . '/wovn_interceptor.php');
 require_once(__DIR__ . '/wovnio/wovnphp/SSI.php');
 use Wovnio\Wovnphp\SSI;
 
+function remove_dots_from_path($path) {
+  # Removes '/./ in paths and resolves '/../' in paths
+  $path = str_replace('//', '/', $path);
+
+  $path_parts = explode('/', $path);
+  $tmp_out = array();
+  foreach($path_parts as $part){
+    if ($part == '.') {
+      continue;
+    }
+    if ($part == '..') {
+      array_pop($tmp_out);
+      continue;
+    }
+    $tmp_out[] = $part;
+  }
+  return implode('/', $tmp_out);
+}
+
 function wovn_helper_detect_paths($base_dir, $path_of_url, $files) {
   $request_path = $base_dir . $path_of_url;
-  $local_path = realpath($request_path);
-  $inside_base_dir = $local_path && strpos($local_path, $base_dir) === 0;
-  $local_path = $inside_base_dir ? $local_path : $base_dir;
+  $local_path = remove_dots_from_path($request_path);
+
   if (is_file($local_path)) {
     return array($local_path);
   } else if (is_dir($local_path)) {
