31cf893c7ab6c6fd4a4639f61b007c35d17f590f
Tim Mahrt <tim@wovn.io>
2018-08-28 12:03:51 +0900

    Update documentation in index_sample for SSI users

--- wovn_index_sample.php
+++ wovn_index_sample.php
@@ -14,6 +14,7 @@ $files = array(
 );
 $paths = wovn_helper_detect_paths(dirname(__FILE__), parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), $files);
 # SSI USER: please swap comments on the two lines below
+# (also see the SSI comment below)
 $included = wovn_helper_include_by_paths($paths);
 # $included = wovn_helper_include_by_paths_with_ssi($paths);
 
@@ -23,7 +24,10 @@ if (!$included) {
 
   # Look for 404.html file in the root directory
   $paths_404 = array(dirname(__FILE__) . "/404.html");
+
+  # SSI USER: please swap comments on the two lines below as you did above
   $included_404 = wovn_helper_include_by_paths($paths_404);
+  # $included_404 = wovn_helper_include_by_paths_with_ssi($paths_404);
 
   if (!$included_404) {
     echo "Page Not Found";
