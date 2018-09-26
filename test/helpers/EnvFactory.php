83e161641dadcfeba6e3a5558f1fe31174388eab
Adrien Bougouin <adrien@wovn.io>
2018-09-26 17:16:43 +0900

    add missing file

---
+++ test/helpers/EnvFactory.php
@@ -0,0 +1,19 @@
+<?php
+namespace Wovnio\Test\Helpers;
+
+class EnvFactory {
+  public static function fromFixture($fixture = 'default', $envOverwrite = array()) {
+    $iniFilename = self::fixture2Filename($fixture);
+    $iniFile = parse_ini_file(dirname(__FILE__) . '/../fixtures/env/' . $iniFilename);
+
+    return array_merge($iniFile['env'], $envOverwrite);
+  }
+
+  private static function fixture2Filename($fixture) {
+    if ($fixture) {
+      return preg_replace('/(.ini)?$/', '.ini', $fixture);
+    }
+
+    return 'default.ini';
+  }
+}
