bfa8513e8cb2ed2bbbef5b8a36f732e9ae1d3995
Adrien Bougouin <adrien@wovn.io>
2019-06-13 09:44:49 +0900

    fix integration tests

--- src/version.php
+++ src/version.php
@@ -1,3 +1,7 @@
 <?php
-define('WOVN_PHP_NAME', 'WOVN.php');
-define('WOVN_PHP_VERSION', '0.1.11');
+if (!defined('WOVN_PHP_NAME')) {
+    define('WOVN_PHP_NAME', 'WOVN.php');
+}
+if (!defined('WOVN_PHP_VERSION')) {
+    define('WOVN_PHP_VERSION', '0.1.11');
+}
