d5e6e959543258e5630526f1423f25cd7c9ffa53
Soichiro Matsumura <soichiro@wovn.io>
2019-07-08 19:44:09 +0900

    add tests

--- test/helpers/CurlMock.php
+++ test/helpers/CurlMock.php
@@ -3,37 +3,47 @@
 // redefine function in the scope of Wovnio\Wovnphp objects.
 namespace Wovnio\Utils\RequestHandlers;
 
-$mock_curl = false;
-$extension_loaded = null;
-$get_extension_funcs = null;
-$curl_version = null;
+$is_curl_mocked = false;
+$mocked_extension_loaded = null;
+$mocked_get_extension_funcs = null;
+$mocked_curl_version = null;
 
 /** MOCK HELPERS **************************************************************/
 
 function mockCurl($curl_loaded, $curl_functions, $curl_protocols)
 {
-    global $mock_curl;
-    global $extension_loaded;
-    global $get_extension_funcs;
-    global $curl_version;
+    global $is_curl_mocked, $mocked_extension_loaded, $mocked_get_extension_funcs, $mocked_curl_version;
 
-    $mock_curl = true;
-    $extension_loaded = $curl_loaded;
-    $get_extension_funcs = $curl_functions;
-    $curl_version = array('protocols' => $curl_protocols);
+    $is_curl_mocked = true;
+    $mocked_extension_loaded = $curl_loaded;
+    $mocked_get_extension_funcs = $curl_functions;
+    $mocked_curl_version = array('protocols' => $curl_protocols);
 }
 
 function restoreCurl()
 {
-    global $mock_curl;
-    global $extension_loaded;
-    global $get_extension_funcs;
-    global $curl_version;
+    global $is_curl_mocked, $mocked_extension_loaded, $mocked_get_extension_funcs, $mocked_curl_version;
 
-    $mock_curl = false;
-    $extension_loaded = null;
-    $get_extension_funcs = null;
-    $curl_version = null;
+    $is_curl_mocked = false;
+    $mocked_extension_loaded = null;
+    $mocked_get_extension_funcs = null;
+    $mocked_curl_version = null;
+}
+
+function mockCurlExec($curl_response, $callback)
+{
+    global $is_curl_exec_mocked, $mocked_curl_response, $curl_exec_callback;
+    $is_curl_exec_mocked = true;
+    $mocked_curl_response = $curl_response;
+    $curl_exec_callback = $callback;
+}
+
+function restoreCurlExec()
+{
+    global $is_curl_exec_mocked, $mocked_curl_response, $curl_exec_callback;
+    $is_curl_exec_mocked = false;
+    $mocked_curl_response = null;
+    $curl_exec_callback = null;
 }
 
 /** MOCKED FUNCTIONS **********************************************************/
@@ -42,25 +52,20 @@ function restoreCurl()
 
 function extension_loaded()
 {
-    global $mock_curl;
-    global $extension_loaded;
-
-    return $mock_curl ? $extension_loaded : call_user_func_array('\curl_loaded', func_get_args());
+    global $is_curl_mocked, $mocked_extension_loaded;
+    return $is_curl_mocked ? $mocked_extension_loaded : call_user_func_array('\curl_loaded', func_get_args());
 }
 
 function get_extension_funcs()
 {
-    global $mock_curl;
-    global $get_extension_funcs;
-
-    return $mock_curl ? $get_extension_funcs : call_user_func_array('\get_extension_funcs', func_get_args());
+    global $is_curl_mocked, $mocked_get_extension_funcs;
+    return $is_curl_mocked ? $mocked_get_extension_funcs : call_user_func_array('\get_extension_funcs', func_get_args());
 }
 
 function curl_version()
 {
-    global $mock_curl;
-    global $curl_version;
-
-    return $mock_curl ? $curl_version : call_user_func_array('\curl_version', func_get_args());
+    global $is_curl_mocked, $mocked_curl_version;
+    return $is_curl_mocked ? $mocked_curl_version : call_user_func_array('\curl_version', func_get_args());
 }
+
 // phpcs:enable
--- test/unit/utils/request_handlers/FileGetContentsRequestHandlerTest.php
+++ test/unit/utils/request_handlers/FileGetContentsRequestHandlerTest.php
@@ -1,10 +1,19 @@
 <?php
-namespace Wovnio\Wovnphp\Tests\Unit\Utils\RequestHandlers;
+namespace Wovnio\Utils\RequestHandlers;
 
 require_once 'src/wovnio/utils/request_handlers/FileGetContentsRequestHandler.php';
+require_once 'test/helpers/FileGetContentsMock.php';
+
+use Wovnio\Utils\RequestHandlers\FileGetContentsRequestHandler;
 
 class FileGetContentsRequestHandlerTest extends \PHPUnit_Framework_TestCase
 {
+    protected function tearDown()
+    {
+        parent::tearDown();
+        restoreFileGetContents();
+    }
+
     public function testPost()
     {
         $api_url = 'http://api.wovn.io/a/b';
@@ -54,5 +63,89 @@ class FileGetContentsRequestHandlerTest extends \PHPUnit_Framework_TestCase
             ->willReturn(array($response, null, null));
 
         return $file_get_handler;
+    }
+
+    private function assertPostRequest($file_get_contents_response, $expected_response, $expected_headers, $expected_error)
+    {
+        $sut = $this->getMock('Wovnio\Utils\RequestHandlers\FileGetContentsRequestHandler', array('fileGetContents'));
+        $body = '<html><head></head><body><h1>Congratulations!</h1></body></html>';
+        $data = array(
+            'url' => 'https://api.wovn.io/v0/translations/?cache_key=12232015',
+            'token' => 'v0Y3u',
+            'lang_code' => 'fr',
+            'url_pattern' => 'path',
+            'lang_param_name' => 'wovn',
+            'product' => WOVN_PHP_NAME,
+            'version' => WOVN_PHP_VERSION,
+            'body' => $body
+        );
+
+        $that = &$this;
+        $sut->expects($this->once())
+			->method('fileGetContents')
+			->will($this->returnCallback(function ($uri, $http_context) use (&$that, &$data, &$file_get_contents_response, &$expected_headers) {
+                $http_context_array = stream_context_get_options($http_context)['http'];
+                $http_headers = explode("\r\n", $http_context_array['header']);
+                $formatted_data = http_build_query($data);
+                $compressed_data = gzencode($formatted_data);
+                $content_length = strlen($compressed_data);
+                $expected_http_headers = array(
+                    'Content-Type: application/octet-stream',
+                    "Content-Length: $content_length",
+                    'Accept-Encoding: gzip'
+                );
+
+                $that->assertEquals($data['url'], $uri);
+                $that->assertEquals($expected_http_headers, $http_headers);
+                $that->assertEquals('POST', $http_context_array['method']);
+                $that->assertEquals($compressed_data, $http_context_array['content']);
+                $that->assertEquals(1, $http_context_array['timeout']);
+
+                return array($file_get_contents_response, $expected_headers);
+            }));
+
+        list($response, $response_headers, $response_error) = $sut->sendRequest('POST', $data['url'], $data, 1);
+
+        $this->assertEquals($expected_response, $response);
+        $this->assertEquals($expected_headers, $response_headers);
+        $this->assertEquals($expected_error, $response_error);
+    }
+
+    public function testAvailable()
+    {
+        mockFileGetContents(true);
+        $this->assertTrue(FileGetContentsRequestHandler::available());
+    }
+
+    public function testNotAvailable()
+    {
+        mockFileGetContents(false);
+        $this->assertFalse(FileGetContentsRequestHandler::available());
+    }
+
+    public function testSendRequestPost()
+    {
+        $formatted_response = array(
+            'body' =>'<html><head></head><body><h1>Félicitations !</h1></body></html>'
+        );
+        $expected_response = json_encode($formatted_response);
+        $compressed_response = gzencode($expected_response);
+        $expected_headers = array(
+            'HTTP/1.0 200',
+            'Content-Encoding: gzip'
+        );
+
+        $this->assertPostRequest($compressed_response, $expected_response, $expected_headers, null);
+    }
+
+    public function testSendRequestPostWithError()
+    {
+        $error_code = 500;
+        $error_headers = array(
+            "HTTP/1.0 $error_code"
+        );
+        $expected_error = "[fgc] Request failed ($error_code)";
+
+        $this->assertPostRequest(false, null, $error_headers, $expected_error);
     }
 }
