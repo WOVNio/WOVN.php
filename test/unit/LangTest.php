61811713d03a949595bdaca8d714ab3bcd1fc288
Soichiro725 <41978013+Soichiro725@users.noreply.github.com>
2020-04-27 18:11:14 +0900

    [RegionCode]Update lang codes with region code (#106)
    ### Purpose/goal of PR (Issue #)
    This PR will update language code with region code based on the following code.
    https://github.com/WOVNio/equalizer/blob/acf54b543369d3aa8acaff6ce7b150d8c00e6e9f/front/src/utils/lang.js
    
    PHP needs lang code formatted by `iso639-1`.
    I implemented like html-swapper.
    html-swapper just changes language code for the followings.
    - zh-CHT -> zh-Hant
    - zh-CHS -> zh-Hans
    
    ### Comments
    Once, I thought that I need to change the following sample RewriteRule.
    But almost all user doesn't need this change.
    So, I will not change about RewriteRule.
    `RewriteRule ^/?(?:ar|eu|bn|bg|ca|zh-CHS|zh-CHT|da|nl|en|fi|fr|gl|de|el|he|hu|id|it|ja|ko|lv|ms|my|ne|no|fa|pl|pt|ru|es|sw|sv|tl|th|hi|tr|uk|vi)($|/.*$) $1 [L]`
    
    
    I will keep current version, because previous fix is not released yet.
    https://github.com/WOVNio/WOVN.php/pull/105

--- test/unit/LangTest.php
+++ test/unit/LangTest.php
@@ -13,7 +13,7 @@ class LangTest extends \PHPUnit_Framework_TestCase
 
     public function testLangLength()
     {
-        $this->assertEquals(40, count(Lang::$index));
+        $this->assertEquals(74, count(Lang::$index));
     }
 
     public function testKeysExist()
@@ -64,45 +64,17 @@ class LangTest extends \PHPUnit_Framework_TestCase
 
     public function testISO6391Normalization()
     {
-        $this->assertEquals('ar', Lang::iso6391Normalization('ar'));
-        $this->assertEquals('eu', Lang::iso6391Normalization('eu'));
-        $this->assertEquals('bn', Lang::iso6391Normalization('bn'));
-        $this->assertEquals('bg', Lang::iso6391Normalization('bg'));
-        $this->assertEquals('ca', Lang::iso6391Normalization('ca'));
-        $this->assertEquals('zh-Hans', Lang::iso6391Normalization('zh-CHS'));
-        $this->assertEquals('zh-Hant', Lang::iso6391Normalization('zh-CHT'));
-        $this->assertEquals('da', Lang::iso6391Normalization('da'));
-        $this->assertEquals('nl', Lang::iso6391Normalization('nl'));
-        $this->assertEquals('en', Lang::iso6391Normalization('en'));
-        $this->assertEquals('fi', Lang::iso6391Normalization('fi'));
-        $this->assertEquals('fr', Lang::iso6391Normalization('fr'));
-        $this->assertEquals('gl', Lang::iso6391Normalization('gl'));
-        $this->assertEquals('de', Lang::iso6391Normalization('de'));
-        $this->assertEquals('el', Lang::iso6391Normalization('el'));
-        $this->assertEquals('he', Lang::iso6391Normalization('he'));
-        $this->assertEquals('hu', Lang::iso6391Normalization('hu'));
-        $this->assertEquals('id', Lang::iso6391Normalization('id'));
-        $this->assertEquals('it', Lang::iso6391Normalization('it'));
-        $this->assertEquals('ja', Lang::iso6391Normalization('ja'));
-        $this->assertEquals('ko', Lang::iso6391Normalization('ko'));
-        $this->assertEquals('lv', Lang::iso6391Normalization('lv'));
-        $this->assertEquals('ms', Lang::iso6391Normalization('ms'));
-        $this->assertEquals('my', Lang::iso6391Normalization('my'));
-        $this->assertEquals('ne', Lang::iso6391Normalization('ne'));
-        $this->assertEquals('no', Lang::iso6391Normalization('no'));
-        $this->assertEquals('fa', Lang::iso6391Normalization('fa'));
-        $this->assertEquals('pl', Lang::iso6391Normalization('pl'));
-        $this->assertEquals('pt', Lang::iso6391Normalization('pt'));
-        $this->assertEquals('ru', Lang::iso6391Normalization('ru'));
-        $this->assertEquals('es', Lang::iso6391Normalization('es'));
-        $this->assertEquals('sw', Lang::iso6391Normalization('sw'));
-        $this->assertEquals('sv', Lang::iso6391Normalization('sv'));
-        $this->assertEquals('th', Lang::iso6391Normalization('th'));
-        $this->assertEquals('hi', Lang::iso6391Normalization('hi'));
-        $this->assertEquals('tr', Lang::iso6391Normalization('tr'));
-        $this->assertEquals('uk', Lang::iso6391Normalization('uk'));
-        $this->assertEquals('ur', Lang::iso6391Normalization('ur'));
-        $this->assertEquals('vi', Lang::iso6391Normalization('vi'));
+        // iso6391 is same with lang code
+        foreach (Lang::$index as $langCode => $lang) {
+            if ($langCode == 'zh-CHS') {
+                $this->assertEquals('zh-Hans', Lang::iso6391Normalization($langCode));
+            } elseif ($langCode == 'zh-CHT') {
+                $this->assertEquals('zh-Hant', Lang::iso6391Normalization($langCode));
+            } else {
+                $this->assertEquals($langCode, Lang::iso6391Normalization($langCode));
+            }
+        }
         $this->assertEquals(null, Lang::iso6391Normalization('fake'));
+        $this->assertEquals(null, Lang::iso6391Normalization(null));
     }
 }
