6fb5fb01571d432894510de2cff54ed29a7fee0f
Yuji Iwai <iwai@wovn.io>
2020-06-10 22:55:19 +0900

    Added dummy container

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -11,9 +11,11 @@ else
     mod_rewrite_activation="${mod_rewrite_activation}; apache2-foreground"
 fi
 
+docker create -v /opt --name $dummy_container $docker_name /bin/true
+docker cp $(pwd) $dummy_container:/opt/project
+
 docker run -d -w /var/www/html \
-       -v $(pwd):/opt/project \
-       -v $(pwd)/test/docroot:/var/www/html \
+       --volumes-from $dummy_container \
        $docker_name /bin/bash -c "${mod_rewrite_activation}"
 
 APACHE_CONTAINER_ID=$(docker ps -q)
@@ -21,8 +23,10 @@ APACHE_CONTAINER_ID=$(docker ps -q)
 function cleanup_container()
 {
     docker stop ${APACHE_CONTAINER_ID} && docker rm ${APACHE_CONTAINER_ID}
+    docker rm $dummy_container
 }
 trap cleanup_container EXIT
 
+docker exec -w /opt/project ${APACHE_CONTAINER_ID} /bin/bash -c "set -e; ln -s /var/www/html /opt/project/test/docroot"
 docker exec -w /opt/project ${APACHE_CONTAINER_ID} /bin/bash -c "set -e; /opt/project/vendor/bin/phpunit --configuration phpunit_integration.xml"
 
