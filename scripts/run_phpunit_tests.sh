0ce04bbf6a172773512023faac24055062dd5273
Yuji Iwai <iwai@wovn.io>
2020-06-11 23:31:59 +0900

    tweak paths

--- scripts/run_phpunit_tests.sh
+++ scripts/run_phpunit_tests.sh
@@ -4,24 +4,28 @@ set -eux
 docker_name=$1
 dummy_container="dummy_$(date +%s)"
 
+VOLUME=/opt
+WORK_DIR=${VOLUME}/project
+PHPUNIT_OUTDIR=.phpunit
+
 # Create a dummy container which will hold a volume with source
 docker create -v /opt --name $dummy_container $docker_name /bin/true
 # Copy source to dummy container
-docker cp $(pwd) $dummy_container:/opt/project
+docker cp $(pwd) $dummy_container:${WORK_DIR}
 
 # Check syntax
-docker run --rm -t -w /opt/project --volumes-from $dummy_container $docker_name \
+docker run --rm -t -w ${WORK_DIR} --volumes-from $dummy_container $docker_name \
        /bin/bash -c 'a=$(find /opt/project -type f -name "*.php" !  -path "*/vendor/*" -print0 | xargs -0 -n 1 -P 8 php -l | grep -v "No syntax errors" | wc -l) && exit $a'
 
 # Run unit test
 if [[ "${docker_name}" =~ ^php:7.*$ ]]; then
-    docker run -t -w /opt/project --volumes-from $dummy_container $docker_name \
-           /bin/bash -c "set -e; phpdbg -qrr vendor/bin/phpunit --log-junit .phpunit/junit.xml -d memory_limit=1024M --coverage-html .phpunit/coverage-report"
-    docker cp $dummy_container:/opt/project/.phpunit ${PWD}/
+    docker run -t -w ${WORK_DIR} --volumes-from $dummy_container $docker_name \
+           /bin/bash -c "set -e; phpdbg -qrr vendor/bin/phpunit --log-junit ${PHPUNIT_OUTDIR}/phpunit/results.xml -d memory_limit=1024M --coverage-html ${PHPUNIT_OUTDIR}/coverage-report"
+    docker cp $dummy_container:"${WORK_DIR}/${PHPUNIT_OUTDIR}" ${PWD}/
 else
-    docker run -t -w /opt/project --volumes-from $dummy_container $docker_name \
-           /bin/bash -c "set -e; vendor/bin/phpunit --log-junit .phpunit/junit.xml"
-    docker cp $dummy_container:/opt/project/.phpunit ${PWD}/
+    docker run -t -w ${WORK_DIR} --volumes-from $dummy_container $docker_name \
+           /bin/bash -c "set -e; vendor/bin/phpunit --log-junit ${PHPUNIT_OUTDIR}/phpunit/results.xml"
+    docker cp $dummy_container:"${WORK_DIR}/${PHPUNIT_OUTDIR}" ${PWD}/
 fi
 
 # Replace for Integration test
@@ -58,6 +62,5 @@ trap cleanup_container EXIT
 
 # Run integration test
 docker exec -w /opt/project ${APACHE_CONTAINER_ID} \
-       /bin/bash -c "set -e; ln -s /var/www/html /opt/project/test/docroot && vendor/bin/phpunit --configuration phpunit_integration.xml --log-junit .phpunit/junit.integration.xml"
-docker cp ${APACHE_CONTAINER_ID}:/opt/project/.phpunit/junit.integration.xml ${PWD}/.phpunit/junit.integration.xml
-
+       /bin/bash -c "set -e; ln -s /var/www/html /opt/project/test/docroot && vendor/bin/phpunit --configuration phpunit_integration.xml --log-junit ${PHPUNIT_OUTDIR}/phpunit/junit.integration.xml"
+docker cp ${APACHE_CONTAINER_ID}:${WORK_DIR}/${PHPUNIT_OUTDIR}/phpunit/junit.integration.xml ${PWD}/${PHPUNIT_OUTDIR}/phpunit/junit.integration.xml
