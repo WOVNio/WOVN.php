2e952ec22431c680a2eddaf3d9415fb1109da6a1
Yuji Iwai <iwai@wovn.io>
2020-06-11 23:42:13 +0900

    Rename junit results.xml

--- scripts/run_phpunit_tests.sh
+++ scripts/run_phpunit_tests.sh
@@ -62,5 +62,5 @@ trap cleanup_container EXIT
 
 # Run integration test
 docker exec -w /opt/project ${APACHE_CONTAINER_ID} \
-       /bin/bash -c "set -e; ln -s /var/www/html /opt/project/test/docroot && vendor/bin/phpunit --configuration phpunit_integration.xml --log-junit ${PHPUNIT_OUTDIR}/phpunit.integration/results.xml"
-docker cp ${APACHE_CONTAINER_ID}:${WORK_DIR}/${PHPUNIT_OUTDIR}/phpunit/junit.integration.xml ${PWD}/${PHPUNIT_OUTDIR}/phpunit/junit.integration.xml
+       /bin/bash -c "set -e; ln -s /var/www/html /opt/project/test/docroot && vendor/bin/phpunit --configuration phpunit_integration.xml --log-junit ${PHPUNIT_OUTDIR}/results.xml"
+docker cp ${APACHE_CONTAINER_ID}:${WORK_DIR}/${PHPUNIT_OUTDIR}/results.xml ${PWD}/${PHPUNIT_OUTDIR}/phpunit.integration/results.xml
