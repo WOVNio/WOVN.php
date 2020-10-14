49225cb01d59c7e18fe04f5c2d90dfc46aabf3be
Zeyu Wu <zeyu.wu@wovn.io>
2020-10-14 18:04:48 +0900

    Adds default value for path.

--- src/wovnio/wovnphp/custom_domain/CustomDomainLangSource.php
+++ src/wovnio/wovnphp/custom_domain/CustomDomainLangSource.php
@@ -9,6 +9,7 @@ class CustomDomainLangSource extends CustomDomainLang
     {
         $url = preg_match("/https?:\/\//", $url, $matches) ? $url : 'http://' . $url;
         $parsedUrl = parse_url($url);
-        parent::__construct($parsedUrl['host'], $parsedUrl['path'], $lang);
+        $path = isset($parsedUrl['path']) ? $parsedUrl['path'] : '/';
+        parent::__construct($parsedUrl['host'], $path, $lang);
     }
 }
