26241bb5720e69690a5ad5b1cf3435d79b93117b
Zeyu Wu <zeyu.wu@wovn.io>
2021-01-05 18:22:04 +0900

    bump patch version

--- src/version.php
+++ src/version.php
@@ -3,6 +3,6 @@ if (!defined('WOVN_PHP_NAME')) {
     define('WOVN_PHP_NAME', 'WOVN.php');
 }
 if (!defined('WOVN_PHP_VERSION')) {
-    $version = isset($_ENV['WOVN_ENV']) && $_ENV['WOVN_ENV'] === 'development' ? 'VERSION' : '1.8.0';
+    $version = isset($_ENV['WOVN_ENV']) && $_ENV['WOVN_ENV'] === 'development' ? 'VERSION' : '1.8.1';
     define('WOVN_PHP_VERSION', $version);
 }
