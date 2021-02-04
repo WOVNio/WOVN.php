d73340408337c29929c1785b3f99521e8df814a7
Soichiro Matsumura <soichiro@wovn.io>
2021-02-04 18:21:54 +0900

    re-install modules only for PHP8

--- scripts/run_integration_tests.sh
+++ scripts/run_integration_tests.sh
@@ -3,7 +3,7 @@
 set -eux
 DOCKER_IMAGE=$1
 NEW_DOCKER_IMAGE=wovnphp_${DOCKER_IMAGE}
-CONTAINER_NAME="dummy"
+CONTAINER_NAME="dummy_$(date +%s)"
 WORK_DIR=/opt/project
 INTGTEST_REPORT_DIR=.phpunit/phpunit.integration
 
@@ -25,10 +25,19 @@ docker build --build-arg DOCKER_IMAGE=${DOCKER_IMAGE} -t ${NEW_DOCKER_IMAGE} ./d
 APACHE_CONTAINER_ID=`docker run -d -e WOVN_ENV=development --name ${CONTAINER_NAME} ${NEW_DOCKER_IMAGE} /bin/bash -c "${MOD_REWRITE_ACTIVATION}; ${START_APACHE}"`
 docker cp $(pwd) ${APACHE_CONTAINER_ID}:${WORK_DIR}
 
-# Install modules
-docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; php ./scripts/composer-setup.php"
-docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; ./composer.phar --version"
-docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; ./composer.phar install"
+if [[ "${DOCKER_IMAGE}" =~ ^php:[8].*$ ]]; then
+    # Convert test to support PHP8 syntax
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "find ${WORK_DIR}/test -type f -name \"*.php\" -print0 | xargs -0 sed -i \"s/function setUp(.*)/function setUp(): void/g\""
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "find ${WORK_DIR}/test -type f -name \"*.php\" -print0 | xargs -0 sed -i \"s/function tearDown(.*)/function tearDown(): void/g\""
+
+    # Remove modules to install modules for PHP8
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; rm -rf vendor"
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; rm composer.lock"
+
+    # Install modules
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; php ./scripts/composer-setup.php"
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; ./composer.phar install"
+fi
 
 # Run integration test
 docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; set -e; vendor/bin/phpunit --configuration phpunit_integration.xml --log-junit ${INTGTEST_REPORT_DIR}/results.xml"
--- scripts/run_phpunit_tests.sh
+++ scripts/run_phpunit_tests.sh
@@ -3,7 +3,7 @@
 set -eux
 DOCKER_IMAGE=$1
 NEW_DOCKER_IMAGE=wovnphp_${DOCKER_IMAGE}
-CONTAINER_NAME="dummy"
+CONTAINER_NAME="dummy_$(date +%s)"
 WORK_DIR=/opt/project
 UNITTEST_REPORT_DIR=.phpunit/phpunit
 
@@ -17,22 +17,31 @@ docker build --build-arg DOCKER_IMAGE=${DOCKER_IMAGE} -t ${NEW_DOCKER_IMAGE} ./d
 APACHE_CONTAINER_ID=`docker run -itd -e WOVN_ENV=development --name ${CONTAINER_NAME} ${NEW_DOCKER_IMAGE} /bin/bash`
 docker cp $(pwd) ${APACHE_CONTAINER_ID}:${WORK_DIR}
 
-# Install modules
-docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; php ./scripts/composer-setup.php"
-docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; ./composer.phar --version"
-docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; ./composer.phar install"
+if [[ "${DOCKER_IMAGE}" =~ ^php:[8].*$ ]]; then
+    # Convert test to support PHP8 syntax
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "find ${WORK_DIR}/test -type f -name \"*.php\" -print0 | xargs -0 sed -i \"s/function setUp(.*)/function setUp(): void/g\""
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "find ${WORK_DIR}/test -type f -name \"*.php\" -print0 | xargs -0 sed -i \"s/function tearDown(.*)/function tearDown(): void/g\""
+
+    # Remove modules to install modules for PHP8
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; rm -rf vendor"
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; rm composer.lock"
+
+    # Install modules
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; php ./scripts/composer-setup.php"
+    docker exec ${APACHE_CONTAINER_ID} /bin/bash -c "cd ${WORK_DIR}; ./composer.phar install"
+fi
 
 # Check syntax
 docker exec ${APACHE_CONTAINER_ID} \
-       /bin/bash -c 'a=$(find /opt/project -type f -name "*.php" ! -path "*/vendor/*" -print0 | xargs -0 -n 1 -P 8 php -l | grep -v "No syntax errors" | wc -l) && exit $a'
+    /bin/bash -c 'a=$(find /opt/project -type f -name "*.php" ! -path "*/vendor/*" -print0 | xargs -0 -n 1 -P 8 php -l | grep -v "No syntax errors" | wc -l) && exit $a'
 
 # Run unit test
 if [[ "${DOCKER_IMAGE}" =~ ^php:7.*$ ]]; then
-       docker exec ${APACHE_CONTAINER_ID} \
-              /bin/bash -c "cd ${WORK_DIR}; set -e; phpdbg -qrr vendor/bin/phpunit --log-junit ${UNITTEST_REPORT_DIR}/results.xml -d memory_limit=1024M --coverage-html ${UNITTEST_REPORT_DIR}/coverage-report"
+    docker exec ${APACHE_CONTAINER_ID} \
+        /bin/bash -c "cd ${WORK_DIR}; set -e; phpdbg -qrr vendor/bin/phpunit --log-junit ${UNITTEST_REPORT_DIR}/results.xml -d memory_limit=1024M --coverage-html ${UNITTEST_REPORT_DIR}/coverage-report"
 else
-       docker exec ${APACHE_CONTAINER_ID} \
-              /bin/bash -c "cd ${WORK_DIR}; set -e; vendor/bin/phpunit --log-junit ${UNITTEST_REPORT_DIR}/results.xml"
+    docker exec ${APACHE_CONTAINER_ID} \
+        /bin/bash -c "cd ${WORK_DIR}; set -e; vendor/bin/phpunit --log-junit ${UNITTEST_REPORT_DIR}/results.xml"
 fi
 
 # Copy test results to host from docker
