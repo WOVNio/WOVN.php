90dc9a8e54daabfe21e33ee560a32d8171b003d0
Soichiro Matsumura <soichiro@wovn.io>
2021-03-22 14:31:18 +0900

    version up to 1.9.1

--- src/version.php
+++ src/version.php
@@ -3,6 +3,6 @@ if (!defined('WOVN_PHP_NAME')) {
     define('WOVN_PHP_NAME', 'WOVN.php');
 }
 if (!defined('WOVN_PHP_VERSION')) {
-    $version = isset($_ENV['WOVN_ENV']) && $_ENV['WOVN_ENV'] === 'development' ? 'VERSION' : '1.9.0';
+    $version = isset($_ENV['WOVN_ENV']) && $_ENV['WOVN_ENV'] === 'development' ? 'VERSION' : '1.9.1';
     define('WOVN_PHP_VERSION', $version);
 }
