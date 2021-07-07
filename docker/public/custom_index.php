54c582e7379be5b089d0c769c708515b192c7bd6
Zeyu Wu <zeyu.wu@wovn.io>
2021-07-07 15:14:51 +0900

    Added support for custom requests.

---
+++ docker/public/custom_index.php
@@ -0,0 +1,12 @@
+<?php
+//require_once('WOVN.php/src/wovn_interceptor.php');
+
+$content = file_get_contents('php://input');
+
+$response_json = json_decode($content, true);
+$response_options = $response_json['response'];
+foreach ($response_options['headers'] as $key => $value) {
+    header("{$key}: {$value}");
+}
+header("Content-Type: {$response_options['content_type']}");
+echo($response_options['body']);
