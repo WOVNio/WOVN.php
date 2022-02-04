a95c0a753723a19ed0c85a7df7db0891a9fb8332
Zeyu Wu <zeyu.wu@wovn.io>
2022-02-04 16:14:56 +0900

    updated canonical tag capture regex

--- test/unit/html/HtmlConverterTest.php
+++ test/unit/html/HtmlConverterTest.php
@@ -308,6 +308,22 @@ class HtmlConverterTest extends TestCase
                 '</html>'
             ),
             array(
+                'with extra attributes variant - should translate existing canonical tag and keep extra attributes',
+
+                '<html><head><link rel="canonical" first="no" href="http://my-site.com/news/" second="yes"></head><body><a>hello</a></body></html>',
+
+                '<html lang="en">' .
+                '<head>' .
+                '<link rel="alternate" hreflang="en" href="http://my-site.com/news/"><link rel="alternate" hreflang="vi" href="http://my-site.com/vi/news/">' .
+                '<script src="//j.wovn.io/1" data-wovnio="key=123456&amp;backend=true&amp;currentLang=vi&amp;defaultLang=en&amp;urlPattern=path&amp;langCodeAliases=[]&amp;langParamName=wovn" data-wovnio-info="version=WOVN.php_VERSION" async></script>' .
+                '<link rel="canonical" first="no" href="http://my-site.com/vi/news/" second="yes">' .
+                '</head>' .
+                '<body>' .
+                '<a>hello</a>' .
+                '</body>' .
+                '</html>'
+            ),
+            array(
                 'canonical URL host does not match request host - should do nothing',
 
                 '<html><head><link rel="canonical" wovn="no" href="http://some-other-site.com/news/"></head><body><a>hello</a></body></html>',
