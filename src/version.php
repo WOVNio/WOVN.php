43aa0eb764818295f26097fedf2aa1837b85c094
bstewart <brett@wovn.io>
2022-06-09 12:58:43 +0900

    patch bump

--- src/version.php
+++ src/version.php
@@ -3,6 +3,6 @@ if (!defined('WOVN_PHP_NAME')) {
     define('WOVN_PHP_NAME', 'WOVN.php');
 }
 if (!defined('WOVN_PHP_VERSION')) {
-    $version = isset($_ENV['WOVN_ENV']) && $_ENV['WOVN_ENV'] === 'development' ? 'VERSION' : '1.14.0';
+    $version = isset($_ENV['WOVN_ENV']) && $_ENV['WOVN_ENV'] === 'development' ? 'VERSION' : '1.14.2';
     define('WOVN_PHP_VERSION', $version);
 }
